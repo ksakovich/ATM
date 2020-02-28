148cd3fb25d92a77086ccabec39025d3b8b565aa
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2020-02-27 20:29:54 -0800

    changed my comment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// Hello world from Kirill
+	// Hello world from Kirill test number 2
         // Nicole was here
         // Olga_test
     }

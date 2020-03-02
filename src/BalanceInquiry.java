0b1e38746d95273a4c6b2b290af122a83709e5ad
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2020-03-01 18:00:47 -0800

    populated NalanceInquiry calss

--- src/BalanceInquiry.java
+++ src/BalanceInquiry.java
@@ -8,6 +8,25 @@ public class BalanceInquiry extends Transaction
     @Override
     public void execute()
     {
+        BankDatabase database = getBankDatabase();
+        Screen screen = getScreen();
+        int currentUserAccountNum = getAccountNumber();
+        double availableBalabce = database.getAvailableBalance(currentUserAccountNum);
+        double totalBalance = database.getAvailableBalance(currentUserAccountNum);
+        if(availableBalabce != totalBalance)
+        {
+            screen.displayMessage("Your available balance is: \n");
+            screen.displayDollarAmount(availableBalabce);
+            screen.displayMessage("\n Your total balance is: \n");
+            screen.displayDollarAmount(totalBalance);
+            screen.displayMessage("\n");
 
+        }
+        else
+        {
+            screen.displayMessage("Your balance is: \n");
+            screen.displayDollarAmount(totalBalance);
+            screen.displayMessage("\n");
+        }
     }
 }

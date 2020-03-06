17d7dcf604a5ef31b20a4e20f6dc6ee6c22857f0
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2020-03-05 22:49:30 -0800

    adjusted start amount to make divisible it by $20

--- src/BankDatabase.java
+++ src/BankDatabase.java
@@ -9,7 +9,7 @@ public class BankDatabase {
         accounts = new Account[4]; // just 2 accounts for testing
         accounts[0] = new Account(12345, 54321, 1000.0, 1200.0);
         accounts[1] = new Account(98765, 56789, 200.0, 200.0);
-        accounts[2] = new Account(1, 1, 50.0, 50.0);
+        accounts[2] = new Account(1, 1, 40.0, 40.0);
         accounts[3] = new Account(2, 2, 20000.0, 20000.0);
     }
 
